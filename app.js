57d8ce71ebe0f2f46955209260b5e2d5be0d78a1
Abdullah Zahra <abdullahmzahra@gmail.com>
2023-02-27 21:25:52 +0200

    Day 32 challenge

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
-let age = 32;
-let userName = "Max";
-let hobbies = ["Sports", "Cooking", "Reading"];
-let job = {
-  title: "Software Engineer",
-  place: "Cairo",
-  salary: 50000,
+// let age = 32;
+// let userName = "Max";
+// let hobbies = ["Sports", "Cooking", "Reading"];
+// let job = {
+//   title: "Software Engineer",
+//   place: "Cairo",
+//   salary: 50000,
+// };
+
+// function calculateAdultYears(userAge) {
+//   return userAge - 18;
+// }
+
+// alert(calculateAdultYears(age));
+
+// age = 45;
+// alert(calculateAdultYears(age));
+
+let courseName = "100 Days of Code - 2023 Web Development Bootcamp";
+let coursePrice = 70;
+let courseGoals = [
+  "To be a great web developer.",
+  "Because I love programming.",
+  "Because I want to learn how the internet works.",
+];
+
+alert(courseName);
+alert(coursePrice);
+alert(courseGoals);
+
+let course = {
+  name: courseName,
+  price: coursePrice,
+  goals: courseGoals,
 };
 
-function calculateAdultYears(userAge) {
-  return userAge - 18;
+let resultName, resultPrice, resultGoals;
+
+function accessCourse() {
+  resultName = course.name;
+  resultPrice = course.price;
+  resultGoals = course.goals;
 }
 
-alert(calculateAdultYears(age));
+accessCourse();
 
-age = 45;
-alert(calculateAdultYears(age));
+alert(resultName);
+alert(resultPrice);
+alert(resultGoals);
