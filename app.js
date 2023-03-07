810eb9bfcd590590789a770076583170ab10c826
Abdullah Zahra <abdullahmzahra@gmail.com>
2023-03-07 07:17:47 +0200

    Day 34 Inserting new HTML Element via JavaScript

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ anchorElement.href = "https://google.com";
 
 anchorElement = document.querySelector("p a"); // p a { color: red; }
 anchorElement.href = "https://linkedin.com";
+
+// ADD AN ELEMENT
+// 1. Create the new element
+
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "google";
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector("p");
+
+// 3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement);
