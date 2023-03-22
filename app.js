0f5b6f25fde55265ee58daef2a7ff610b9d5ab3e
Abdullah Zahra <abdullahmzahra@gmail.com>
2023-03-22 22:35:41 +0200

    Section 18

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/currenttime", function (req, res) {
 
 app.get("/", function (req, res) {
   res.send(
-    '<form action="/store-user" method="POST"><lable>Your Name</lable><input type="text" name="username"><button>Submit</button></form>'
+    '<form action="/store-user" method="POST"><lable>Your Name:</lable><input type="text" name="username"><button>Submit</button></form>'
   );
 }); // localhost:3000/
 
@@ -30,6 +30,23 @@ app.post("/store-user", function (req, res) {
   fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
   res.send("<h1>Username stored!</h1>");
+});
+
+app.get("/users", function (req, res) {
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = "<ul>";
+
+  for (const user of existingUsers) {
+    responseData += "<li>" + user + "</li>";
+  }
+
+  responseData += "</ul>";
+
+  res.send(responseData);
 });
 
 app.listen(3000);
