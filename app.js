a03ca0a9b54075379d7657b697ba6c7d277984b4
Abdullah Zahra <abdullahmzahra@gmail.com>
2023-03-22 21:20:43 +0200

    Day 49

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-function handleRequest(request, response) {
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1>");
-  } else if (request.url === "/") {
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  }
-}
+const express = require("express");
 
-const server = http.createServer(handleRequest);
+const app = express();
 
-server.listen(3000);
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+}); // localhost:3000/currenttime
+
+app.get("/", function (req, res) {
+  res.send(
+    '<form action="/store-user" method="POST"><lable>Your Name</lable><input type="text" name="username"><button>Submit</button></form>'
+  );
+}); // localhost:3000/
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  existingUsers.push(userName);
+
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+
+  res.send("<h1>Username stored!</h1>");
+});
+
+app.listen(3000);
