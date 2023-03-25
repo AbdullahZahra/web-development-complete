1d85877b606f50bca8553468b16ea38145ebd246
Abdullah Zahra <abdullahmzahra@gmail.com>
2023-03-25 06:35:49 +0200

    Section 21

---
+++ async.js
@@ -0,0 +1,24 @@
+const fs = require("fs/promises");
+
+async function readFile() {
+  let fileData;
+
+  //   fs.readFile("data.txt", function (error, fileData) {
+  //     console.log("File parsing done!");
+  //     console.log(fileData.toString());
+  //     // start another async task that sends the data to a database
+  //   });
+
+  try {
+    fileData = await fs.readFile("data.txt");
+  } catch (error) {
+    console.log("An error has occured");
+  }
+
+  console.log("File parsing done!");
+  console.log(fileData.toString());
+  // return anotherAsyncOperation;
+  console.log("Hi there!");
+}
+
+readFile();
