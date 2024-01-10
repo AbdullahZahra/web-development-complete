2d418db758a2875276f85af42772326319269e7f
Abdullah Zahra <abdullahmzahra@gmail.com>
2024-01-10 16:29:41 +0200

    Operator Precedence

--- script.js
+++ script.js
@@ -1,58 +1,13 @@
-/*
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
 
-console.log("Jonas");
-console.log(23);
+console.log(now - 1991 > now - 2018);
 
-let firstName = "Matilda";
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-
-let jonas_matilda = "JM";
-let $function = 27;
-
-let person = "jonas";
-const PI = 3.1415;
-
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
-
-console.log(myFirstJob);
-
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
-
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof "Jonas");
-
-javascriptIsFun = "YES!";
-console.log(typeof javascriptIsFun);
-
-let year;
-console.log(year);
-console.log(typeof year);
-
-year = 1991;
-console.log(typeof year);
-
-console.log(typeof null);
-*/
-
-let age = 30;
-age = 31;
-
-const birthYear = 1991;
-// birthYear = 1990;
-
-// const job;
-
-var job = "porgrammer";
-job = "teacher";
-
-lastName = "Schmedtmann";
-console.log(lastName);
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah);
+console.log(averageAge);
