af7d416552b09c43e44878206fcb2c632628a71b
Abdullah Zahra <abdullahmzahra@gmail.com>
2024-01-17 22:54:11 +0200

    The switch Statement

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const hasDriversLicense = true;
-const hasGoodVision = true;
+const day = "friday";
 
-console.log(hasDriversLicense && hasGoodVision);
-console.log(hasDriversLicense || hasGoodVision);
-console.log(!hasDriversLicense);
-
-// if (hasDriversLicense && hasGoodVision) {
-//   console.log("Sarah is able to drive!");
-// } else {
-//   console.log("Someone else should drive...");
-// }
-
-const isTired = false; //
-console.log(hasDriversLicense && hasGoodVision && isTired);
-
-if (hasDriversLicense && hasGoodVision && !isTired) {
-  console.log("Sarah is able to drive!");
-} else {
-  console.log("Someone else should drive...");
+switch (day) {
+  case "monday": // day === monday
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "Wednesday":
+  case "thursday":
+    console.log("write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day!");
 }
